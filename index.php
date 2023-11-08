cccf5c37d569fd3023364fcbb75f77a3196e633a
Mohamed KADI <handskadi@gmail.com>
2023-11-08 20:08:20 +0000

    home edits

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div id="content">
+	<main>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<article>
+				<h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+			</article>
+		<?php endwhile; endif; ?>
+    </main>
+</div>
+
+<?php get_footer(); ?>
