8ae00446c34180df27366002416ee4f9ee9445fb
Mohamed KADI <handskadi@gmail.com>
2023-11-08 16:44:40 +0000

    Image change on even listener

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-	// Code in this block runs when the DOM is fully loaded
-	var greeting = 'Hello, World!';
-	//alert(greeting);
-});
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+	const mySrc = myImage.getAttribute("src");
+	if (mySrc === "http://192.168.56.3/wp-content/themes/mktheme/screenshot.png") {
+		myImage.setAttribute("src", "http://192.168.56.3/wp-content/themes/twentytwentytwo/screenshot.png");
+	} else {
+		myImage.setAttribute("src", "http://192.168.56.3/wp-content/themes/mktheme/screenshot.png");
+	}
+};
+
